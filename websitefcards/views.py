69e1c0dae3569323519783b2ba0dfc0947ffa355
Thomas Poore <thomas.poore@gmail.com>
2020-11-25 09:16:12 -0500

    initial commit

---
+++ websitefcards/views.py
@@ -0,0 +1,168 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+	return render(request, 'home.html', {})
+
+def add(request):
+	from random import randint
+
+	num_1 = randint(0,10)
+	num_2 = randint(0,10)
+
+	if request.method == "POST":
+		answer = request.POST['answer']
+
+		if not answer:
+			return render(request, 'add.html', {
+			'num_1':num_1,
+			'num_2':num_2
+			})
+
+		old_num_1 = request.POST['old_num_1']
+		old_num_2 = request.POST['old_num_2']
+
+		correct_answer = int(old_num_1) + int(old_num_2)
+
+		if int(answer) == correct_answer:
+			my_answer = "Correct! %s + %s = %s" % (old_num_1,old_num_2,correct_answer)
+			color = 'success'
+		else:
+			my_answer = "Incorrect! %s + %s = %s not %s" % (old_num_1,old_num_2,correct_answer,answer)
+			color = 'danger'
+
+		return render(request, 'add.html', {
+				'answer':answer,
+				'my_answer':my_answer,
+				'num_1':num_1,
+				'num_2':num_2,
+				'color':color,
+				})
+
+	return render(request, 'add.html', {
+		'num_1':num_1,
+		'num_2':num_2
+		})
+
+def subtract(request):
+	from random import randint
+
+	num_1 = randint(0,10)
+	num_2 = randint(0,10)
+
+	if request.method == "POST":
+		answer = request.POST['answer']
+
+		if not answer:
+			return render(request, 'subtract.html', {
+			'num_1':num_1,
+			'num_2':num_2
+			})
+
+		old_num_1 = request.POST['old_num_1']
+		old_num_2 = request.POST['old_num_2']
+
+		correct_answer = int(old_num_1) - int(old_num_2)
+
+		if int(answer) == correct_answer:
+			my_answer = "Correct! %s - %s = %s" % (old_num_1,old_num_2,correct_answer)
+			color = 'success'
+		else:
+			my_answer = "Incorrect! %s - %s = %s not %s" % (old_num_1,old_num_2,correct_answer,answer)
+			color = 'danger'
+
+		return render(request, 'subtract.html', {
+				'answer':answer,
+				'my_answer':my_answer,
+				'num_1':num_1,
+				'num_2':num_2,
+				'color':color,
+				})
+
+	return render(request, 'subtract.html', {
+		'num_1':num_1,
+		'num_2':num_2
+		})
+
+def multiply(request):
+	from random import randint
+
+	num_1 = randint(0,10)
+	num_2 = randint(0,10)
+
+	if request.method == "POST":
+		answer = request.POST['answer']
+
+		if not answer:
+			return render(request, 'multiply.html', {
+			'num_1':num_1,
+			'num_2':num_2
+			})
+
+		old_num_1 = request.POST['old_num_1']
+		old_num_2 = request.POST['old_num_2']
+
+		correct_answer = int(old_num_1) * int(old_num_2)
+
+		if int(answer) == correct_answer:
+			my_answer = "Correct! %s X %s = %s" % (old_num_1,old_num_2,correct_answer)
+			color = 'success'
+		else:
+			my_answer = "Incorrect! %s X %s = %s not %s" % (old_num_1,old_num_2,correct_answer,answer)
+			color = 'danger'
+
+		return render(request, 'multiply.html', {
+				'answer':answer,
+				'my_answer':my_answer,
+				'num_1':num_1,
+				'num_2':num_2,
+				'color':color,
+				})
+
+	return render(request, 'multiply.html', {
+		'num_1':num_1,
+		'num_2':num_2
+		})
+
+
+def divide(request):
+	from random import randint
+
+	num_1 = randint(0,10)
+	num_2 = randint(1,10)
+
+	if request.method == "POST":
+		answer = request.POST['answer']
+
+		if not answer:
+			return render(request, 'divide.html', {
+			'num_1':num_1,
+			'num_2':num_2
+			})
+
+
+		old_num_1 = request.POST['old_num_1']
+		old_num_2 = request.POST['old_num_2']
+
+		correct_answer = int(old_num_1) / int(old_num_2)
+
+		if float(answer) == correct_answer:
+			my_answer = "Correct! %s / %s = %s" % (old_num_1,old_num_2,correct_answer)
+			color = 'success'
+		else:
+			my_answer = "Incorrect! %s / %s = %s not %s" % (old_num_1,old_num_2,correct_answer,answer)
+			color = 'danger'
+
+		return render(request, 'divide.html', {
+				'answer':answer,
+				'my_answer':my_answer,
+				'num_1':num_1,
+				'num_2':num_2,
+				'color':color,
+				})
+
+	return render(request, 'divide.html', {
+		'num_1':num_1,
+		'num_2':num_2
+		})
